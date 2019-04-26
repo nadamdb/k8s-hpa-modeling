df92651d99b479fef82ab44c89435e1dbd69a652
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2019-04-26 16:03:58 +0200

    Implement delta 0th derivate order with possibly wrong terminating condition

--- continuous-model/continuous_model.py
+++ continuous-model/continuous_model.py
@@ -1,4 +1,7 @@
-import exception_classes as ex
+import itertools
+import sys
+
+sys.setrecursionlimit(100000)
 
 
 class MMc_WeightedBusyTimeAnalyzer(object):
@@ -13,11 +16,13 @@ class MMc_WeightedBusyTimeAnalyzer(object):
         """
         self.n2 = n2
         self.n1 = n1
+        if c < 2:
+            raise ValueError("c must be at least 2")
         self.c = c
         self.mu_ = mu_
         self.lambda_ = lambda_
-        if lambda_ < c * mu_:
-            raise ex.UnboundedExpectedValue()
+        if lambda_ >= c * mu_:
+            raise ValueError("lamba < c * mu must be satisfied, otherwise expected value is unbounded")
 
     def p_dep(self, k):
         """
@@ -27,7 +32,7 @@ class MMc_WeightedBusyTimeAnalyzer(object):
         :return:
         """
         if 1 > k > self.c:
-            raise ex.KOutofRange()
+            raise ValueError("k must be between 1 and c")
         return k * self.mu_ / (self.lambda_ + k * self.mu_)
 
     def p_arr(self, k):
@@ -38,7 +43,7 @@ class MMc_WeightedBusyTimeAnalyzer(object):
         :return:
         """
         if 1 > k > self.c:
-            raise ex.KOutofRange()
+            raise ValueError("k must be between 1 and c")
         return self.lambda_ / (self.lambda_ + k * self.mu_)
 
     def func_delta_der0(self, k, s, current_n1=None, current_n2=None):
@@ -55,14 +60,58 @@ class MMc_WeightedBusyTimeAnalyzer(object):
             current_n1 = self.n1
         if current_n2 is None:
             current_n2 = self.n2
+        if k < 1 or k > self.c:
+            raise ValueError("k must be between 1 and c")
+        final_solution_list = []
         if k == 1:
-            return
+            # the inner recursion is collecting all 1 --> 2 transitions, which is described by n2.
+            if current_n2 == 0:
+                if current_n1 == 0:
+                    # self.func_upsilon(k, s) * (self.p_dep(k) + self.p_arr(k)), but the second == 1 always
+                    return [self.func_upsilon(1, s)]
+                else:
+                    # we have reached the bottom (k==1), taken enough 1 --> 2 transitions into account for this single 0 --> 1 transition.
+                    possible_solutions_prev_rec = self.func_delta_der0(1, s, current_n1 - 1, self.n2)
+                    for delta_value in possible_solutions_prev_rec:
+                         final_solution_list.append( self.func_upsilon(1, s) * (self.p_dep(1) * delta_value + self.p_arr(1)) )
+                    return final_solution_list
+            else:
+                if current_n1 == 0:
+                    # we have reached the bottom (k==1), but we still need to consider some more 1 --> 2 transitions for this last 0 -->
+                    # 1 transition.
+                    possible_solutions_prev_rec = self.func_delta_der0(2, s, 0, current_n2 - 1)
+                    for delta_value in possible_solutions_prev_rec:
+                        final_solution_list.append( self.func_upsilon(1, s) * (self.p_dep(1) + self.p_arr(1) * delta_value) )
+                    return final_solution_list
+                else:
+                    # we have reached the bottom (k==1), but we still need to consider some more 1 --> 2 transitions and some more 0 -->
+                    # 1 transitions.
+                    # so we decrease the desired number of 1 --> 2 transitions.
+                    possible_solutions_prev_rec_1_2 = self.func_delta_der0(2, s, current_n1, current_n2 - 1)
+                    # so we decrease the desired number of 0 --> 1 transitions.
+                    possible_solutions_prev_rec_0_1 = self.func_delta_der0(1, s, current_n1 - 1, current_n2)
+                    for delta_trans_0_1, delta_trans_1_2 in itertools.product(possible_solutions_prev_rec_0_1,
+                                                                              possible_solutions_prev_rec_1_2):
+                        final_solution_list.append(self.func_upsilon(1, s) * (self.p_dep(1) * delta_trans_0_1 +
+                                                                              self.p_arr(1) * delta_trans_1_2))
+                    return final_solution_list
         if k == self.c:
-            # TODO: this can have multiple values based on the 0th order derivate of etha_c (s). we cannot discard values here,
+            # this can have multiple values based on the 0th order derivate of etha_c (s). we cannot discard values here,
             # only we have the negativity / positivity constraints at the 1st and 2nd order derivates of delta.
-            return
-        # else: 1 < k < c
-        return
+            etha_c_values = self.func_etha_c(self.c * s)
+            # Multiple values might come from evaluating the delta
+            # we didn't reach bottom, n1, n2 stays the same.
+            possible_solutions_prev_rec = self.func_delta_der0(self.c - 1, s, current_n1, current_n2)
+            for etha_c_value, delta_value in itertools.product(etha_c_values, possible_solutions_prev_rec):
+                final_solution_list.append(etha_c_value * delta_value / float(self.c))
+            return final_solution_list
+        else:
+            # else: 1 < k < c
+            possible_solutions_arr = self.func_delta_der0(k + 1, s, current_n1, current_n2)
+            possible_solutions_dep = self.func_delta_der0(k - 1, s, current_n1, current_n2)
+            for delta_arr, delta_dep in itertools.product(possible_solutions_arr, possible_solutions_dep):
+                final_solution_list.append(self.func_upsilon(k, k * s) / float(k) * (self.p_dep(k) * delta_dep + self.p_arr(k) * delta_arr))
+            return final_solution_list
 
     def func_delta_der1(self):
         # NOTE: 1 or multiple functions based on derivate order??
@@ -83,7 +132,7 @@ class MMc_WeightedBusyTimeAnalyzer(object):
         elif derivate_order == 2:
             return 2 / ((s + self.lambda_ + k * self.mu_) ** 3)
         else:
-            raise ex.UnsupportedArgumentValue()
+            raise ValueError("Higher derivate order is not supported")
 
     def func_etha_c(self, s, derivate_order=0):
         """
@@ -95,13 +144,22 @@ class MMc_WeightedBusyTimeAnalyzer(object):
         :return: list of values, each of them being a possible solution
         """
         if s != 0:
-            raise ex.UnsupportedArgumentValue()
+            raise ValueError("s can only be 0")
         if derivate_order == 0:
             return sorted([self.c * self.mu_ / self.lambda_,
                            1.0])
         elif derivate_order == 1:
+            # NOTE : the second element is POSITIV, its (-1)* cannot be the extected value of T_c
             return sorted([1 / (self.lambda_ - self.c * self.mu_),
                            self.c * self.mu_ / (self.lambda_ * self.c * self.mu_ - (self.lambda_ ** 2))])
         elif derivate_order == 2:
-            # TODO (NEXT!):
-            return
+            # NOTE: the 4th element is Negative, it cannot be the second moment of T_c
+            return sorted([2 * self.c * self.mu_ / ((self.c * self.mu_ - self.lambda_) ** 3),
+                           2 * ((self.c * self.mu_) ** 2) / self.lambda_ / ((self.c * self.mu_ - self.lambda_) ** 3),
+                           2 * self.lambda_ / ((self.lambda_ - self.c * self.mu_) ** 2),
+                           -2 * self.c * self.mu_ * self.lambda_ / ((self.c * self.mu_ - self.lambda_) ** 2)])
+
+
+if __name__ == '__main__':
+    analyzer = MMc_WeightedBusyTimeAnalyzer(lambda_=2, mu_=11, c=2)
+    print analyzer.func_delta_der0(k=2, s=0)
